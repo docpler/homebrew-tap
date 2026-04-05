93be8935057b70d9c296e29db712df2cbc0be5d8
chiang <chiang@rightstack.net>
2026-04-05 21:03:02 +0900

    Update to cli-v1.0.1

--- Formula/docpler.rb
+++ Formula/docpler.rb
@@ -1,22 +1,22 @@
 class Docpler < Formula
   desc "Convert HWP (Korean word processor) documents to Markdown"
   homepage "https://github.com/docpler/docpler-cli"
-  version "1.0.0"
+  version "1.0.1"
   license "BSL-1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/docpler/docpler-cli/releases/download/cli-v1.0.0/docpler-aarch64-apple-darwin"
-      sha256 "1be840ae63ae330a1aa9403c14ba9b480e550e395279724038d0eca4148efbea"
+      url "https://github.com/docpler/docpler-cli/releases/download/cli-v1.0.1/docpler-aarch64-apple-darwin"
+      sha256 "afc7bed194a7a1ba8aef6680944a046ec883aaaac6ccc7a852f632b702819de1"
     else
-      url "https://github.com/docpler/docpler-cli/releases/download/cli-v1.0.0/docpler-x86_64-apple-darwin"
-      sha256 "5cebefb75a6e14854f41dde32dc322001871882a6fd8fc5aa05744f9bc50005c"
+      url "https://github.com/docpler/docpler-cli/releases/download/cli-v1.0.1/docpler-x86_64-apple-darwin"
+      sha256 "edba24a00ca9f6f2b5cae7356f5e9fb5dcfced788fb13b69d715dcbb7f40fed1"
     end
   end
 
   on_linux do
-    url "https://github.com/docpler/docpler-cli/releases/download/cli-v1.0.0/docpler-x86_64-unknown-linux-gnu"
-    sha256 "9da0ca8fe8b9bb7f81547c90326631ee168b3ad6de06e4ceb5e69c8397bb935e"
+    url "https://github.com/docpler/docpler-cli/releases/download/cli-v1.0.1/docpler-x86_64-unknown-linux-gnu"
+    sha256 "a2c7df1327167b9c262c59390768d93937ee1d47847ae24e39953943bcc0a7f4"
   end
 
   def install
